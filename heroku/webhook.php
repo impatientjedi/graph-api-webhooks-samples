9251c721018c93dedb7becd3f8b0e82b7731820f
impatientjedi <115368999+impatientjedi@users.noreply.github.com>
2023-08-11 04:02:45 -0600

    Update webhook.php

--- heroku/webhook.php
+++ heroku/webhook.php
@@ -18,5 +18,36 @@ function getWebhookData($reqBody) {
   );
 }
 
+function hook() {
+
+  $ch = curl_init('http://hooks.wearpanda.com/lister.php');
+
+   $ch = curl_init();
+
+  // Set the URL of the webhook.
+  curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+
+  // Set the method to POST.
+  curl_setopt($ch, CURLOPT_POST, 1);
+
+  // Set the content type to application/json.
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+  // Set the body of the request to the webhook data.
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($reqBody));
+
+  // Execute the cURL request.
+  curl_exec($ch);
+
+  // Check for errors.
+  if (curl_errno($ch)) {
+    echo 'cURL error: ' . curl_error($ch);
+  }
+
+  // Close the cURL handle.
+  curl_close($ch);
+  
+}
+
 ?>
 
